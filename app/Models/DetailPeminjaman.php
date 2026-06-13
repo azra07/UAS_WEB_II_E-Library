1d11e5175166a42e7c1b99d99f379e70fb633999
Muhammad Rizky Akbar <2410817210016@mhs.ulm.ac.id>
2026-06-14 06:53:10 +0800

    Update DetailPeminjaman.php

--- app/Models/DetailPeminjaman.php
+++ app/Models/DetailPeminjaman.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class DetailPeminjaman extends Model
 {
-    //
+    protected $table = 'detail_peminjaman'; // Explicitly state the table name
+    protected $fillable = ['borrow_id', 'book_id'];
+
+    public function book() {
+        return $this->belongsTo(Book::class);
+    }
 }
