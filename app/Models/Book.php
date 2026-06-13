724d9a2e297b953b452ca03bbaf990060cbc6ac1
Muhammad Rizky Akbar <2410817210016@mhs.ulm.ac.id>
2026-06-14 05:40:20 +0800

    Update Book.php

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,15 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-// Tambahkan ini agar error hilang:
 use App\Models\Category;
 use App\Models\Publisher;
 use App\Models\Rating;
 
 class Book extends Model
 {
-    // Supaya bisa diisi data lewat form, tambahkan baris ini:
-    protected $fillable = ['judul', 'kategori_id', 'publisher_id'];
+    protected $fillable = [
+        'judul', 
+        'penulis', 
+        'isbn', 
+        'category_id', 
+        'publisher_id'
+    ];
 
     public function category() { return $this->belongsTo(Category::class); }
     public function publisher() { return $this->belongsTo(Publisher::class); }
