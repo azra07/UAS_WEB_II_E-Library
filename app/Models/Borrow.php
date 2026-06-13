a8f593227ec3588d385ba78f1cbf5392d4375f2d
Muhammad Rizky Akbar <2410817210016@mhs.ulm.ac.id>
2026-06-14 06:52:55 +0800

    Update Borrow.php

--- app/Models/Borrow.php
+++ app/Models/Borrow.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Borrow extends Model
 {
-    //
+    protected $fillable = ['user_id', 'tanggal_pinjam', 'due_date', 'tanggal_kembali'];
+
+    protected $casts = [
+        'tanggal_pinjam' => 'datetime',
+        'due_date' => 'datetime',
+        'tanggal_kembali' => 'datetime',
+    ];
+
+    public function user() { return $this->belongsTo(User::class); }
+    public function details() { return $this->hasMany(DetailPeminjaman::class); }
 }
