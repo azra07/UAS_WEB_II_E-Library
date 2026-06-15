e1eb503f9eed06eb7d4044f6a29dab7f3d7023fe
azra07 <136769399+azra07@users.noreply.github.com>
2026-06-16 02:20:02 +0800

    Add fillable properties and relationships to Rating model

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'book_id',
+        'user_id',
+        'rating',
+        'ulasan' // <-- Ubah menjadi 'ulasan' sesuai kolom database
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class, 'book_id');
+    }
 }
