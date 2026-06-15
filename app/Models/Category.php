0afa3318bf6bb93dd9953ccdcd214d09366a0017
azra07 <136769399+azra07@users.noreply.github.com>
2026-06-16 03:09:28 +0800

    Refactor Category model to include books relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Book; // Import model Book di atas
 
 class Category extends Model
 {
-    protected $fillable = ['nama_kategori', 'deskripsi'];
+    use HasFactory;
+
+    protected $fillable = [
+        'nama_kategori',
+        'deskripsi'
+    ];
+
+    /**
+     * RELASI PENTING: Satu Kategori memiliki banyak Buku (One to Many)
+     */
+    public function books()
+    {
+        return $this->hasMany(Book::class, 'category_id');
+    }
 }
